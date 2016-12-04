483bc0ea91aebf969537e7a8b163016d5d3fce9b
1312070 <1312070@student.hcmus.edu.vn>
2016-12-04 14:50:39 +0700

    Gop search vao action Agent va Merchant

--- WebMVC/Controllers/ManagementController.cs
+++ WebMVC/Controllers/ManagementController.cs
@@ -28,23 +28,51 @@ namespace WebMVC.Controllers
             return View();
         }
         [HttpGet]
-        public ActionResult Agent(int page = 1,int size = 10)
+        public ActionResult Agent(string searchString, int page = 1,int size = 10)
         {
             IList<AGENT> list = new List<AGENT>();
+            var model = Session[CommonConstants.USER_SESSION];
+            var temp = new USER_INFORMATION();
+            if (model != null)
+            {
+                temp = (USER_INFORMATION)model;
+            }
+            else return View("Index");
            
             //HttpClient client = new HttpClient();
             //client.BaseAddress = new Uri("http://localhost:21212/");
 
             //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpClient client = new AccessAPI().Access();
-            HttpResponseMessage response = client.GetAsync(string.Format("api/Agent/FindAllAgent")).Result;
-
-            if (response.IsSuccessStatusCode)
+            if (temp.UserType == "T")
             {
-                list = response.Content.ReadAsAsync<List<AGENT>>().Result;
+                HttpClient client = new AccessAPI().Access();
+                @ViewBag.action = "Agent";
+                if (String.IsNullOrEmpty(searchString))
+                {
+                    HttpResponseMessage response = client.GetAsync(string.Format("api/Agent/FindAllAgent")).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        list = response.Content.ReadAsAsync<List<AGENT>>().Result;
+                    }
+                    var listAgent = list.ToPagedList(page, size);
+                    return View(listAgent);
+                }
+                else
+                {
+                    HttpResponseMessage response = client.GetAsync(string.Format("api/Agent/FindAgentElement?searchString={0}", searchString)).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        list = response.Content.ReadAsAsync<List<AGENT>>().Result;
+                    }
+                    @ViewBag.searchString = searchString;
+                    var listAgent = list.ToPagedList(page, size);
+                    return View(listAgent);
+                }
             }
-            var listAgent = list.ToPagedList(page, size);
-            return View(listAgent);
+            else return View("Index");
+            
         }
 
         [HttpGet]
@@ -130,7 +158,7 @@ namespace WebMVC.Controllers
         }
 
         [HttpGet]
-        public ActionResult Merchant(int page = 1, int size = 10)
+        public ActionResult Merchant(string searchString, int page = 1, int size = 10)
         {
             List<MERCHANT> list = new List<MERCHANT>();
             var model = Session[CommonConstants.USER_SESSION];
@@ -141,30 +169,61 @@ namespace WebMVC.Controllers
             }
             else return View("Index");
             HttpClient client = new AccessAPI().Access();
-            
+            @ViewBag.action = "Merchant";
             if (temp.UserType == "T")
             {
-                HttpResponseMessage response = client.GetAsync(string.Format("api/Merchant/FindAllMerchant")).Result;
-                if (response.IsSuccessStatusCode)
+                if (String.IsNullOrEmpty(searchString))
                 {
-                    list = response.Content.ReadAsAsync<List<MERCHANT>>().Result;
-                }
-                
-                var listMerchant = list.ToPagedList(page, size);
+                    HttpResponseMessage response = client.GetAsync(string.Format("api/Merchant/FindAllMerchant")).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        list = response.Content.ReadAsAsync<List<MERCHANT>>().Result;
+                    }
 
-                return View(listMerchant); 
+                    var listMerchant = list.ToPagedList(page, size);
+
+                    return View(listMerchant);
+                }
+                else
+                {
+                    HttpResponseMessage response = client.GetAsync(string.Format("api/Merchant/FindMerchantElement?searchString={0}", searchString)).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        list = response.Content.ReadAsAsync<List<MERCHANT>>().Result;
+                    }
+                    @ViewBag.searchString = searchString;
+                    var listMerchant = list.ToPagedList(page, size);
+                    return View(listMerchant);
+                } 
             }
             else
             {
                 if (temp.UserType == "A")
                 {
-                    HttpResponseMessage response = client.GetAsync(string.Format("api/Merchant/FindMerchantByAgentCode?agentCode={0}", temp.UserName)).Result;
-                    if (response.IsSuccessStatusCode)
+                    if (String.IsNullOrEmpty(searchString))
                     {
-                        list = response.Content.ReadAsAsync<List<MERCHANT>>().Result;
+                        HttpResponseMessage response = client.GetAsync(string.Format("api/Merchant/FindMerchantByAgentCode?agentCode={0}", temp.UserName)).Result;
+                        if (response.IsSuccessStatusCode)
+                        {
+                            list = response.Content.ReadAsAsync<List<MERCHANT>>().Result;
+                        }
+                        var listMerchant = list.ToPagedList(page, size);
+                        return View(listMerchant);
                     }
-                    var listMerchant = list.ToPagedList(page, size);
-                    return View(listMerchant);
+                    else
+                    {
+                        HttpResponseMessage response = client.GetAsync(string.Format("api/Merchant/FindMerchantByAgentCodeAndElement?searchString={0}&agentCode={1}", searchString, temp.UserName)).Result;
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            list = response.Content.ReadAsAsync<List<MERCHANT>>().Result;
+                        }
+                        @ViewBag.searchString = searchString;
+                        @ViewBag.agentCode = temp.UserName;
+                        var listMerchant = list.ToPagedList(page, size);
+                        return View(listMerchant);
+                    }
                 }
                 else return View("Index");
             }
