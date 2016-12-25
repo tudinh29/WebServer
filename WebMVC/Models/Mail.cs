23530d0665fe07da939304664451f2a84563e14b
DESKTOP-1H5NO01\THANLV <lvthank13it@gmail.com>
2016-12-25 23:58:57 +0700

    Hoàn thành xong phần gửi email ( đã build và test )

---
+++ WebMVC/Models/Mail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models
+{
+    public class Mail
+    {
+        public string MailMaster { get; set; }
+        public string MailMerchant { get; set; }
+        public string Password { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public string MerchantInfor { get; set; }
+    }
+}
