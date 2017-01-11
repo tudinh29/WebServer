758b657b58b01bf6142fffe3f0d7d95514a82fad
1312071 <1312071@student.hcmus.edu.vn>
2017-01-11 13:31:09 -0800

    chinh sua print Trans_invalid

--- WebAPI/Controllers/TRANSACTION_DETAIL_INVALIDController.cs
+++ WebAPI/Controllers/TRANSACTION_DETAIL_INVALIDController.cs
@@ -14,6 +14,25 @@ namespace WebAPI.Controllers
         private APIDbContext db = new APIDbContext();
         //============================MASTER======================================
         [HttpGet]
+        public List<Models.TransInvalidTiny> FindAllTransaction_Detail_Invalid()
+        {
+            var res = db.Database.SqlQuery<Models.TransInvalidTiny>("sp_FindAllTransaction_Detail_Invalid").ToList();
+            return res;
+        }
+        [HttpGet]
+        public List<Models.TransInvalidTiny> FindTransInvalidElement(string searchString)
+        {
+            object[] parameter = 
+                {
+                    new SqlParameter("@Element", searchString)
+                   
+                };
+            var res = db.Database.SqlQuery<Models.TransInvalidTiny>("sp_FindTransInvalidElement @Element", parameter).ToList();
+            return res;
+        }
+
+
+        [HttpGet]
         public int CountTransInvalid_Master()
         {
             var res = db.Database.SqlQuery<int>("sp_CountTransInvalid_Master").ToList();
@@ -51,6 +70,31 @@ namespace WebAPI.Controllers
         }
         //============================================================================
         //==========================AGENT=============================================
+        [HttpGet]
+        public List<Models.TransInvalidTiny> FindTransInvalidElement_Agent(string searchString, string agentCode)
+        {
+            object[] parameter = 
+                {
+                    new SqlParameter("@Element", searchString),
+                    new SqlParameter("@AgentCode", agentCode)
+                   
+                };
+            var res = db.Database.SqlQuery<Models.TransInvalidTiny>("sp_FindTransInvalidElement_Agent @Element, @AgentCode", parameter).ToList();
+            return res;
+        }
+
+        [HttpGet]
+        public List<Models.TransInvalidTiny> FindTransInvalid_Agent(string AgentCode)
+        {
+            object[] parameter = 
+                {
+                    new SqlParameter("@AgentCode", AgentCode)
+                };
+            var res = db.Database.SqlQuery<Models.TransInvalidTiny>("sp_TransInvalid_Agent @AgentCode", parameter).ToList();
+            return res;
+
+        }
+        
         [HttpGet]
         public int CountTransInvalid_Agent(string AgentCode)
         {
@@ -100,6 +144,30 @@ namespace WebAPI.Controllers
         }
         //============================================================================
         //========================MERCHANT============================================
+        [HttpGet]
+        public List<Models.TransInvalidTiny> FindTransInvalidElement_Merchant(string searchString, string merchantCode)
+        {
+            object[] parameter = 
+                {
+                    new SqlParameter("@Element", searchString),
+                    new SqlParameter("@MerchantCode", merchantCode)
+                   
+                };
+            var res = db.Database.SqlQuery<Models.TransInvalidTiny>("sp_FindTransInvalidElement_Merchant @Element, @MerchantCode", parameter).ToList();
+            return res;
+        }
+
+        [HttpGet]
+        public List<Models.TransInvalidTiny> FindTransInvalid_Merchant(string MerchantCode)
+        {
+            object[] parameter = 
+                {
+                    new SqlParameter("@MerchantCode", MerchantCode)
+                };
+            var res = db.Database.SqlQuery<Models.TransInvalidTiny>("sp_TransInvalid_Merchant @MerchantCode", parameter).ToList();
+            return res;
+
+        }
         [HttpGet]
         public int CountTransInvalid_Merchant(string MerchantCode)
         {
